b030ec9b42c0a401763774524960c7c61293d114
Yannick Scherer <yannick.scherer@gmail.com>
2013-04-18 16:38:13 +0200

    Using thriftclj-0.1.0-alpha6

--- src/clj/client.clj
+++ src/clj/client.clj
@@ -13,8 +13,8 @@
   [client cmd args]
   (case cmd
     "store" (let [[id f l a] args
-                  id (Integer/parseInt id)
-                  a (Integer/parseInt a)
+                  id (Integer/parseInt (str id))
+                  a (Integer/parseInt (str a))
                   p (Person. f l a)]
               (info "Storing Person using ID" id "...")
               (let [r (PersonIndex/storePerson client id p)]
@@ -22,7 +22,7 @@
                   (info "Storing successful.")
                   (warn "Storing failed. (Duplicate ID?)"))))
     "get" (let [[id] args
-                id (Integer/parseInt id)]
+                id (Integer/parseInt (str id))]
             (info "Retrieving Person using ID" id "...")
             (let [p (PersonIndex/getPerson client id)]
               (info "Person:" p)))
@@ -33,13 +33,13 @@
   [& args]
   (let [[data params help] (cli args
                                 ["-h" "--host" "Host to connect to." :default "localhost"]
-                                ["-p" "--port" "Port to connect to." :parse-fn #(Integer. %) :default 7007]
+                                ["-p" "--port" "Port to connect to." :parse-fn #(Integer. ^String %) :default 7007]
                                 ["-c" "--command" "Command to execute."])
         {:keys[host port command]} data]
     (if command
       (do
         (info "Connecting to" (str host ":" port) "...")
-        (with-open [c (thrift/connect! PersonIndex [:socket host port])]
+        (with-open [c (thrift/connect! PersonIndex [host port])]
           (run-command c command params))
         (info "Done."))
       (println help))))
